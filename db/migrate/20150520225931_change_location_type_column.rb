68aa441a25d6f34e7a6912cabc8c919375b43034
Samuel Stewart <stewasc3@student.op.ac.nz>
2015-05-21 16:11:03 +1200

    Change migration to satisfy postgres

--- db/migrate/20150520225931_change_location_type_column.rb
+++ db/migrate/20150520225931_change_location_type_column.rb
@@ -1,5 +1,6 @@
 class ChangeLocationTypeColumn < ActiveRecord::Migration
   def change
     change_column :locations, :location_type, :integer
+    change_column :locations, :location_type, 'integer USING CAST("location_type" AS integer)'
   end
 end
