3f1eeef299a279c8b7d04ad32d659de9a723ee12
Samuel Stewart <samuelcstewart@icloud.com>
2015-08-30 00:55:48 +1200

    Remove presence validation for blurb, add for location_id and animal_id

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,7 +2,8 @@ class Report < ActiveRecord::Base
   belongs_to :location
   belongs_to :animal
   validates :geolocation, :presence => true
-  validates :blurb, :presence => true
+  validates :location_id, :presence => true
+  validates :animal_id, :presence => true
 
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_presence :image
