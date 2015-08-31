9f6284848444dcd000f57a1e24b3ffa9e062491a
Samuel Stewart <samuelcstewart@icloud.com>
2015-08-31 21:35:15 +1200

    Remove commented sections in location controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -10,42 +10,6 @@ class LocationsController < ApplicationController
     render json: @location
   end
 
-  # Other controller actions unapplicable.
-
-  # POST /locations
-  # POST /locations.json
-=begin
-  def create
-    @location = Location.new(location_params)
-
-    if @location.save
-      render json: @location, status: :created, location: @location
-    else
-      render json: @location.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /locations/1
-  # PATCH/PUT /locations/1.json
-  def update
-    @location = Location.find(params[:id])
-
-    if @location.update(location_params)
-      head :no_content
-    else
-      render json: @location.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /locations/1
-  # DELETE /locations/1.json
-  def destroy
-    @location.destroy
-
-    head :no_content
-  end
-=end
-
   private
 
     def set_location
